c7d831348d17e6793c1544b516c5b7f2a29264ca
junyupL <levifox54@gmail.com>
2022-10-16 17:24:43 -0700

    added barely working list

--- heap.go
+++ heap.go
@@ -14,17 +14,69 @@ func sizeof[T any]() C.ulonglong {
 	return C.ulonglong(unsafe.Sizeof(a))
 }
 
-func new[T any]() *T {
-	return (*T)(C.malloc(sizeof[T]()))
+func new[T any](sizes ...C.ulonglong) *T {
+	var factor C.ulonglong = 1
+	for _, size := range sizes {
+		factor *= size
+	}
+	return (*T)(C.malloc(factor * sizeof[T]()))
 }
 
 func delete[T any](ptr *T) {
 	C.free(unsafe.Pointer(ptr))
 }
 
+type Node[T any] struct {
+    data T
+	next *Node[T]
+}
+
+func (base *Node[T])insert(index int, d T) {
+	if index == 0 {
+		p_node := new[Node[T]]()
+		*p_node = *base
+		base.data = d
+		base.next = p_node
+		return
+	}
+	var curr *Node[T] = base
+	for i := 1; i < index; i++ {
+		curr = curr.next
+		if curr == nil {
+			//index out of bounds
+			return
+		}
+	}
+	p_node := new[Node[T]]()
+	p_node.data = d
+	if curr.next == nil {
+		
+		p_node.next = nil
+		curr.next = p_node
+
+	} else {
+		p_node.next = curr.next
+		curr.next = p_node
+	}
+
+}
+func (base Node[T]) destruct() {
+	for curr := base.next; curr != nil; {
+		temp := curr.next
+		delete(curr)
+		curr = temp
+		
+	}
+}
+
+
 func main() {
-	var p = new[int]()
-	*p = 4
-	fmt.Print(*p)
-	delete(p)
+	var l Node[int]
+	l.data = 1
+	l.insert(0, 4)
+	l.insert(2, 7)
+	l.insert(1, 10)
+	
+	fmt.Print(l.data, l.next.data, l.next.next.data, l.next.next.next.data)
+	l.destruct()
 }
