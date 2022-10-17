d6cad487b0e0a4c669c2075c7fc3c204f8f259d6
junyupL <levifox54@gmail.com>
2022-10-16 20:35:53 -0700

    fix list implementation

--- heap.go
+++ heap.go
@@ -31,37 +31,41 @@ type Node[T any] struct {
 	next *Node[T]
 }
 
-func (base *Node[T])insert(index int, d T) {
-	if index == 0 {
-		p_node := new[Node[T]]()
-		*p_node = *base
-		base.data = d
-		base.next = p_node
+func insert[T any](p_list **Node[T], index int, d T) {
+	if index < 0 {
+		//index out of bounds
 		return
 	}
-	var curr *Node[T] = base
+
+	if index == 0 {
+		temp := *p_list
+		*p_list = new[Node[T]]()
+		(*p_list).data = d
+		(*p_list).next = temp
+		return
+	}
+	//pointer to node to add the new node after
+	var to_add *Node[T] = *p_list
+	if to_add == nil {
+		//index out of bounds
+		return
+	}
 	for i := 1; i < index; i++ {
-		curr = curr.next
-		if curr == nil {
+		to_add = to_add.next
+		if to_add == nil {
 			//index out of bounds
 			return
 		}
 	}
 	p_node := new[Node[T]]()
 	p_node.data = d
-	if curr.next == nil {
-		
-		p_node.next = nil
-		curr.next = p_node
-
-	} else {
-		p_node.next = curr.next
-		curr.next = p_node
-	}
+	p_node.next = to_add.next
+	to_add.next = p_node
+	
 
 }
-func (base Node[T]) destruct() {
-	for curr := base.next; curr != nil; {
+func (p_node *Node[T]) destruct() {
+	for curr := p_node; curr != nil; {
 		temp := curr.next
 		delete(curr)
 		curr = temp
@@ -71,12 +75,15 @@ func (base Node[T]) destruct() {
 
 
 func main() {
-	var l Node[int]
-	l.data = 1
-	l.insert(0, 4)
-	l.insert(2, 7)
-	l.insert(1, 10)
+	var linkedl *Node[int] = nil
+	insert(&linkedl, 0, 1)
+	insert(&linkedl, 0, 4)
+	insert(&linkedl, 2, 7)
+	insert(&linkedl, 1, 10)
 	
-	fmt.Print(l.data, l.next.data, l.next.next.data, l.next.next.next.data)
-	l.destruct()
+	for p_node := linkedl; p_node != nil; p_node = p_node.next {
+		fmt.Print(p_node.data, " ")
+	}
+	
+	linkedl.destruct()
 }
